973ad01d1e3a472a537563b00d5cce2dd2c65455
BarisAkkus <barisakkus351@gmail.com>
2021-05-16 18:59:14 +0300

    master

---
+++ Ders20adventure.py
@@ -0,0 +1,6 @@
+available_exist = ["north","south","east","west"]
+
+chosen_exit = ""
+while chosen_exit not in available_exist:
+    chosen_exit = input("Please choose a direction: ")
+print("aren't you glad you got out of there ")
