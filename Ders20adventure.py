9223e490fb7512b1e2040410effaf8670b48fffa
BarisAkkus <barisakkus351@gmail.com>
2021-05-16 19:19:29 +0300

    master

--- Ders20adventure.py
+++ Ders20adventure.py
@@ -3,4 +3,8 @@ available_exist = ["north","south","east","west"]
 chosen_exit = ""
 while chosen_exit not in available_exist:
     chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() == "quit":
+        print("Game over ")
+        break
+    
 print("aren't you glad you got out of there ")
