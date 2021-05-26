9457866013011675fa7633827b5a2c81818c6947
BarisAkkus <barisakkus351@gmail.com>
2021-05-26 11:40:20 +0300

    master

--- Sezon2/S2D2enumerate_example.py
+++ Sezon2/S2D2enumerate_example.py
@@ -1,3 +1,9 @@
-for index, character in enumerate("abcdefgh"):
-    print(index,character)
-    
+# for index, character in enumerate("abcdefgh"):
+#     print(index,character)
+for t in enumerate("abcdefgh"):
+    index,character = t
+    print(index, character)
+
+index,character = (0,'a')
+print(index)
+print(character)
--- Sezon2/S2D5tuples.py
+++ Sezon2/S2D5tuples.py
@@ -1,19 +1,16 @@
-welcome = "Welcome to my Nightmare","Alice Cooper",1975
-bad = "Bad Comapny", "Bad Company", 1974
-budgie = "Nightflight","Budgie", 1981
-imelda = "More Mayhem","Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica",1984
+albums = [("Welcome to my Nightmare","Alice Cooper",1975),
+        ("Bad Company", "Bad Company", 1974),
+        ("Nightflight","Budgie", 1981),
+        ("More Mayhem","Emilda May", 2011),
+        ("Ride the Lightning", "Metallica",1984),
+         ]
+print(len(albums))
 
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
-
-#metallica[0] = "Master of Puppets"
-#TypeError: 'tuple' object does not support item assignment
-metallica2=list(metallica)
-print(metallica2)
-
-metallica2[0] = "Master of Puppets"
-print(metallica2)
-
-
+for name,artist,year in albums:
+    print("Album : {}, Artist: {}, Year: {}"
+          .format(name,artist,year))
+print()
+for album in albums:
+    name,artist,year = album
+    print("Album : {}, Artist: {}, Year: {}"
+          .format(name,artist,year))
