e156df9915acfc51e4eb38601b842408c840e037
BarisAkkus <barisakkus351@gmail.com>
2021-06-04 16:52:00 +0300

    master

--- Object Orientated/obj3.py
+++ Object Orientated/obj3.py
@@ -4,14 +4,29 @@ class Pet:
         self.age = age
         
     def show(self):
-        print(f"I am{self.name} and I am {self.age} years old")
-
+        print(f"I am {self.name} and I am {self.age} years old")
+    def speak(self):
+        print("I dont know what I say")
 
 
 class Cat(Pet):
+    def __init__(self,name,age,color):
+        super().__init__(name,age)
+        self.color = color
+
     def speak(self):
         print("Meow")
+    
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
     
 class Dog(Pet):
     def speak(self):
         print("Bark")
+
+p = Pet("Tim",19)
+p.speak()
+c = Cat("Bill",34,"Brown")
+c.show()
+d = Dog("Jill",25)
+d.speak()
